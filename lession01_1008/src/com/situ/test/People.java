ab32f1503691701155201e495600f58581812531
lishuo <1344793627@qq.com>
2020-10-08 19:08:23 +0800

    3、定义一个人员的相关信息的变量，如姓名、性别、生日、年龄、邮件、手机、爱好项等，分别提示用户输入，并打印输出相关信息

---
+++ lession01_1008/src/com/situ/test/People.java
@@ -0,0 +1,31 @@
+package com.situ.test;
+
+import javax.xml.crypto.Data;
+
+public class People {
+	private String peopleName;
+	private int peopleAge;
+	private String peopleSex;
+	private int peopleBirthday;
+	private String peoplePhoneNumber;
+	private String peopleEmail;
+	private String peopleHobby;
+	public People(String peopleName, int peopleAge, String peopleSex, int peopleBirthday, String peoplePhoneNumber,
+			String peopleEmail, String peopleHobby) {
+		super();
+		this.peopleName = peopleName;
+		this.peopleAge = peopleAge;
+		this.peopleSex = peopleSex;
+		this.peopleBirthday = peopleBirthday;
+		this.peoplePhoneNumber = peoplePhoneNumber;
+		this.peopleEmail = peopleEmail;
+		this.peopleHobby = peopleHobby;
+	}
+	@Override
+	public String toString() {
+		return "People [peopleName=" + peopleName + ", peopleAge=" + peopleAge + ", peopleSex=" + peopleSex
+				+ ", peopleBirthday=" + peopleBirthday + ", peoplePhoneNumber=" + peoplePhoneNumber + ", peopleEmail="
+				+ peopleEmail + ", peopleHobby=" + peopleHobby + "]";
+	}
+	
+}
---
+++ lession01_1008/src/com/situ/test/PeopleTest.java
@@ -0,0 +1,27 @@
+package com.situ.test;
+
+import java.util.Scanner;
+
+public class PeopleTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入用户姓名");
+		String name = sc.next();
+		System.out.println("请输入用户年龄");
+		int age = sc.nextInt();
+		System.out.println("请输入用户性别");
+		String sex = sc.next();
+		System.out.println("情书用户生日");
+		int birthday = sc.nextInt();
+		System.out.println("请输入用户手机号");
+		String phoneNum = sc.next();
+		System.out.println("请输入用户邮箱地址");
+		String email = sc.next();
+		System.out.println("请输入用户爱好");
+		String hobby = sc.next();
+		People p = new  People(name, age, sex, birthday, phoneNum, email, hobby);
+		System.out.println(p.toString());
+	}
+
+}
