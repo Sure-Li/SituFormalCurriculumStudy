665f0c03fc474e426790797fd5d064d39bb4a7cf
lishuo <1344793627@qq.com>
2020-10-26 10:40:01 +0800

    文件类 课堂小测试完成

--- lession13_1026/src/com/situ/lession13/FileTest.java
+++ lession13_1026/src/com/situ/lession13/FileTest.java
@@ -1,13 +1,25 @@
 package com.situ.lession13;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class FileTest {
 
 	public static void main(String[] args) {
-		File file = new File("src\\com");
-		System.out.println(file.isDirectory());
-		System.out.println(file.getAbsolutePath());
+		File file = new File("src\\com\\situ\\lession13");
+		File[] fileArray = file.listFiles();
+		for (File file2 : fileArray) {
+			if (file2.isDirectory()) {
+				System.out.print("目录：\t");
+			}else if (file2.isFile()) {
+				System.out.print("文件：\t");
+			}
+			System.out.println(file2.getName()+"\t大小    "+(file2.length())/(1024D*1024D)+"\tMB");
+			Date date = new Date(file2.lastModified());
+			
+			System.out.println(new SimpleDateFormat("最后修改时间\t"+"yyyy-MM-dd HH:mm:ss").format(date));
+		}
 	}
-
 }
